1bdcde7977a9ea0c5593c69a5d37e1db33274c31
Ricardo Casa <rchoy@cecropiasolutions.com>
2019-10-29 18:08:27 -0500

    Program create the balls

---
+++ main.js
@@ -0,0 +1,69 @@
+// setup canvas
+
+var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext('2d');
+
+var width = canvas.width = window.innerWidth;
+var height = canvas.height = window.innerHeight;
+
+// function to generate random number
+
+function random(min,max) {
+  var num = Math.floor(Math.random()*(max-min)) + min;
+  return num;
+}
+
+// Creating Ball class
+class Ball {
+    constructor(x, y, velX, velY, color, size){
+        this.x = x;
+        this.y = y;
+        this.velX = velX;
+        this.velY = velY;
+        this.color = color;
+        this.size = size;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x, this.y, this.size, 0, 2*Math.PI);
+        ctx.fill();
+    }
+
+    update() {
+        if ((this.x + this.size) >= width) {
+            this.velX = -(this.velX);
+        }
+
+        if ((this.x - this.size) <= 0) {
+            this.velX = -(this.velX);
+        }
+
+        if ((this.y + this.size) >= height) {
+            this.velY = -(this.velY);
+        }
+
+        if ((this.y + this.size) <= 0) {
+            this.velY = -(this.velY);
+        }
+
+        this.x += this.velX;
+        this.y += this.velY
+    }
+}
+
+var balls = [];
+
+while (balls.length < 25) {
+    var size = random(10,20);
+    var ball = new Ball(
+        random( 0 + size, width - size),
+        random( 0 + size, width - size),
+        random(-7,7),
+        random(-7,7),
+        'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
+        size
+    );
+    balls.push(ball);
+}
