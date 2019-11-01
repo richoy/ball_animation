7c5ceb4b8f99ee09c8a22cc8fd143853bd6fc74a
Ricardo Choy <rchoy@cecropiasolutions.com>
2019-11-01 16:27:06 -0500

    Updates to have the game finished, still need to fix some bugs

--- main.js
+++ main.js
@@ -13,13 +13,20 @@ function random(min,max) {
   return num;
 }
 
-// Creating Ball class
-class Ball {
-    constructor(x, y, velX, velY, color, size){
+class Shape {
+    constructor(x, y, velX, velY, exists){
         this.x = x;
         this.y = y;
         this.velX = velX;
         this.velY = velY;
+        this.exists = exists;
+    }
+}
+
+// Creating Ball class
+class Ball extends Shape {
+    constructor(x, y, velX, velY, color, size, exists){
+        super(x, y, velX, velY, exists);
         this.color = color;
         this.size = size;
     }
@@ -67,9 +74,74 @@ class Ball {
     }
 }
 
+class Evil extends Shape{
+    constructor(x, y, velX, velY, color, size){
+        super(x, y, velX = 20, velY = 20);
+        this.color = 'white';
+        this.size = 20;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.strokeStyle = this.color;
+        ctx.lineWidth = 3;
+        ctx.arc(this.x, this.y, this.size, 0, 2*Math.PI);
+        ctx.stroke();
+    };
+
+    update() {
+        if ((this.x + this.size) >= width) {
+            this.x -= this.size;
+        }
+
+        if ((this.x - this.size) <= 0) {
+            this.x += this.size;
+        }
+
+        if ((this.y + this.size) >= height) {
+            this.y -= this.size;
+        }
+
+        if ((this.y + this.size) <= 0) {
+            this.y += this.size;
+        }
+    };
+
+    setControls() {
+        var _this = this;
+        window.onkeydown = function(e) {
+            if (e.keyCode === 37) {
+                _this.x -= _this.velX;
+              } else if (e.keyCode === 39) {
+                _this.x += _this.velX;
+              } else if (e.keyCode === 38) {
+                _this.y -= _this.velY;
+              } else if (e.keyCode === 40) {
+                _this.y += _this.velY;
+              }
+        }
+    };
+
+    collisionDetect() {
+        for (let j = 0; j < balls.length; j++){
+           if(balls[j].exists === true) {
+                var dx = this.x - balls[j].x;
+                var dy = this.y - balls[j].y;
+                var distance = Math.sqrt(dx * dx + dy * dy);
+
+                if (distance < this.size + balls[j].size) {
+                    balls[j].exists = false;
+                    balls.splice(j,1);
+                }
+           }
+        }
+    }
+}
+
+
 var balls = [];
 
-while (balls.length < 25) {
+while (balls.length < 5) {
     var size = random(10,20);
     var ball = new Ball(
         random( 0 + size, width - size),
@@ -77,23 +149,44 @@ while (balls.length < 25) {
         random(-7,7),
         random(-7,7),
         'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
-        size
+        size,
+        true
     );
     balls.push(ball);
 }
+
+var evilBall = new Evil (20,20,0,0,0,0);
+evilBall.setControls();
 
 function loop() {
     ctx.fillStyle = 'rgba(0,0,0,0.25)';
     ctx.fillRect(0, 0, width, height);
 
-    for (let i = 0; i < balls.length; i++) {
-        balls[i].draw();
-        balls[i].update();
-        balls[i].collisionDetect();
-    }
+/*
+    for (let i = 0; i > balls.length; i++) {
+        if (balls[i].exists === true){
+            balls[i].draw();
+            balls[i].update();
+            balls[i].collisionDetect();
+        }
 
+
+    }
+*/
+
+    balls.forEach(function(e) {
+        if (e.exists === true){
+            e.draw();
+            e.update();
+            e.collisionDetect();
+        }
+
+    });
+    evilBall.draw();
+    evilBall.collisionDetect();
+    evilBall.update();
     requestAnimationFrame(loop);
 
 }
 
-loop();
+//loop();
